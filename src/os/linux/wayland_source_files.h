f93c3f69c0f0d134dfe3655924df076eba93128a
Zachary Thompson <zachthompson406@gmail.com>
2026-05-25 15:19:03 -0700

    remove extra wayland file

--- src/os/linux/wayland_source_files.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "pointer-constraints-unstable-v1.c"
-#include "pointer-warp-v1.c"
-#include "relative-pointer-unstable-v1.c"
-#include "xdg-decoration-unstable-v1.c"
-#include "xdg-output-unstable-v1.c"
-#include "xdg-shell.c"
-#include "xdg-toplevel-icon-v1.c"
